f4820dc1440cfebde2e643292c09d50ee520cb8c
Raymond Bian <raybbian@gmail.com>
2026-03-13 11:06:11 -0400

    csharpier

--- Hypermania/Assets/Scripts/Scenes/Menus/InputSelect/DeviceIcon.cs
+++ Hypermania/Assets/Scripts/Scenes/Menus/InputSelect/DeviceIcon.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 namespace Scenes.Menus.InputSelect
 {
-    public class DeviceIcon: MonoBehaviour
+    public class DeviceIcon : MonoBehaviour
     {
-        [SerializeField] TMP_Text _deviceName;
-        
+        [SerializeField]
+        TMP_Text _deviceName;
+
         public void SetDeviceName(string deviceName)
         {
             _deviceName.text = deviceName;
